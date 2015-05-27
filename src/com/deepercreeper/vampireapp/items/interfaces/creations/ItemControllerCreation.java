511de9e19904ee8912fe0622795e035fc83fcae0
vrl <vrl@rb-lap-vrl.recommind.com>
2015-05-27 17:19:41 +0200

    #87: Added java docs.

--- src/com/deepercreeper/vampireapp/items/interfaces/creations/ItemControllerCreation.java
+++ src/com/deepercreeper/vampireapp/items/interfaces/creations/ItemControllerCreation.java
@@ -2,14 +2,19 @@ package com.deepercreeper.vampireapp.items.interfaces.creations;
 
 import java.util.List;
 import android.content.Context;
-import android.widget.LinearLayout;
 import com.deepercreeper.vampireapp.character.creation.CreationMode;
 import com.deepercreeper.vampireapp.items.interfaces.GroupOption;
 import com.deepercreeper.vampireapp.items.interfaces.ItemController;
 import com.deepercreeper.vampireapp.items.interfaces.ItemGroup;
 import com.deepercreeper.vampireapp.items.interfaces.creations.restrictions.CreationRestrictionable;
+import com.deepercreeper.vampireapp.util.view.Viewable;
 
-public interface ItemControllerCreation extends CreationRestrictionable
+/**
+ * This controller controls several groups and a few group options.
+ * 
+ * @author vrl
+ */
+public interface ItemControllerCreation extends CreationRestrictionable, Viewable
 {
 	/**
 	 * Used to call the number of points from the parent.
@@ -40,18 +45,32 @@ public interface ItemControllerCreation extends CreationRestrictionable
 		public void increase(int aValue);
 	}
 	
+	/**
+	 * Adds a name shortcut for the given item.
+	 * 
+	 * @param aItem
+	 *            The item.
+	 */
 	public void addItemName(ItemCreation aItem);
 	
+	/**
+	 * @param aGroup
+	 *            The group.
+	 * @param aValue
+	 *            The value.
+	 * @return whether the value of the given group can be changed by the given value.
+	 */
 	public boolean canChangeGroupBy(final ItemGroupCreation aGroup, int aValue);
 	
+	/**
+	 * Clears the whole controller.
+	 */
 	public void clear();
 	
 	/**
 	 * Closes the widget container. Maybe for refilling with new values.
 	 */
 	public void close();
-	
-	public LinearLayout getContainer();
 	
 	/**
 	 * @return the context of this value controller.
@@ -68,20 +87,56 @@ public interface ItemControllerCreation extends CreationRestrictionable
 	 */
 	public List<ItemCreation> getDescriptionValues();
 	
+	/**
+	 * @param aGroup
+	 *            The group type.
+	 * @return the group with the given group type.
+	 */
 	public ItemGroupCreation getGroup(ItemGroup aGroup);
 	
+	/**
+	 * @param aName
+	 *            The group name.
+	 * @return the group with the given name.
+	 */
 	public ItemGroupCreation getGroup(String aName);
 	
+	/**
+	 * @param aGroupOption
+	 *            The group option type.
+	 * @return the group option with the given group option type.
+	 */
 	public GroupOptionCreation getGroupOption(GroupOption aGroupOption);
 	
+	/**
+	 * @param aName
+	 *            The group option name.
+	 * @return the group option with the given name.
+	 */
 	public GroupOptionCreation getGroupOption(String aName);
 	
+	/**
+	 * @return a list of all group options.
+	 */
 	public List<GroupOptionCreation> getGroupOptionsList();
 	
+	/**
+	 * @return a list of all groups.
+	 */
 	public List<ItemGroupCreation> getGroupsList();
 	
+	/**
+	 * @param aName
+	 *            The group name.
+	 * @return the value of he group with the given name.
+	 */
 	public int getGroupValue(String aName);
 	
+	/**
+	 * @param aName
+	 *            The item name.
+	 * @return The item with the given name.
+	 */
 	public ItemCreation getItem(final String aName);
 	
 	/**
@@ -89,31 +144,62 @@ public interface ItemControllerCreation extends CreationRestrictionable
 	 */
 	public ItemController getItemController();
 	
+	/**
+	 * @param aName
+	 *            The item name.
+	 * @return the value of the item with the given name.
+	 */
 	public int getItemValue(String aName);
 	
+	/**
+	 * @return the controller name.
+	 */
 	public String getName();
 	
+	/**
+	 * @return the points handler.
+	 */
 	public PointHandler getPoints();
 	
+	/**
+	 * @return the number of temporary points spent inside this controller.
+	 */
 	public int getTempPoints();
 	
+	/**
+	 * @return all values of this controller.
+	 */
 	public int getValue();
 	
+	/**
+	 * @param aName
+	 *            The group name.
+	 * @return whether this controller contains a group with the given name.
+	 */
 	public boolean hasGroup(String aName);
 	
+	/**
+	 * @param aName
+	 * @return whether this controller contains an item with the given name.
+	 */
 	public boolean hasItem(String aName);
 	
-	public void init();
-	
 	/**
-	 * Removes all widgets from their parent container.
+	 * Removes the item with the given name.
+	 * 
+	 * @param aName
+	 *            The item name.
 	 */
-	public void release();
-	
 	public void removeItemName(String aName);
 	
+	/**
+	 * resets all temporary points spent in this controller.
+	 */
 	public void resetTempPoints();
 	
+	/**
+	 * Resizes the view of this controller to the real size.
+	 */
 	public void resize();
 	
 	/**
@@ -132,13 +218,16 @@ public interface ItemControllerCreation extends CreationRestrictionable
 	 */
 	public void setEnabled(boolean aEnabled);
 	
+	/**
+	 * Sets the points handler.
+	 * 
+	 * @param aPoints
+	 *            The new points handler.
+	 */
 	public void setPoints(PointHandler aPoints);
 	
 	/**
 	 * Updates all value groups.
-	 * 
-	 * @param aUpdateOthers
-	 *            Whether all other controllers should also be updated.
 	 */
 	public void updateGroups();
 }
--- src/com/deepercreeper/vampireapp/items/interfaces/creations/ItemGroupCreation.java
+++ src/com/deepercreeper/vampireapp/items/interfaces/creations/ItemGroupCreation.java
@@ -2,28 +2,56 @@ package com.deepercreeper.vampireapp.items.interfaces.creations;
 
 import java.util.List;
 import android.content.Context;
-import android.widget.LinearLayout;
 import com.deepercreeper.vampireapp.character.creation.CreationMode;
 import com.deepercreeper.vampireapp.items.interfaces.Item;
 import com.deepercreeper.vampireapp.items.interfaces.ItemGroup;
 import com.deepercreeper.vampireapp.items.interfaces.creations.ItemControllerCreation.PointHandler;
 import com.deepercreeper.vampireapp.items.interfaces.creations.restrictions.CreationRestrictionable;
+import com.deepercreeper.vampireapp.util.view.Viewable;
 
-public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, CreationRestrictionable
+/**
+ * Used to create item groups.
+ * 
+ * @author vrl
+ */
+public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, CreationRestrictionable, Viewable
 {
+	/**
+	 * Asks the user to choose an item that should be added to this group.
+	 */
 	public void addItem();
 	
+	/**
+	 * Adds an item creation with the given item type.
+	 * 
+	 * @param aItem
+	 *            The item type.
+	 */
 	public void addItem(Item aItem);
 	
+	/**
+	 * @param aValue
+	 *            The value.
+	 * @return whether this groups value can be changed by the given value.
+	 */
 	public boolean canChangeBy(int aValue);
 	
+	/**
+	 * @return whether the child items of this group have a specific order.
+	 */
 	public boolean hasOrder();
 	
+	/**
+	 * Clears all child items and properties of this group.
+	 */
 	public void clear();
 	
+	/**
+	 * Replaces the given child item with an user chosen one.
+	 * 
+	 * @param aItem
+	 */
 	public void editItem(Item aItem);
-	
-	public LinearLayout getContainer();
 	
 	/**
 	 * @return the context of this value group.
@@ -40,6 +68,11 @@ public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, Creati
 	 */
 	public List<ItemCreation> getDescriptionItems();
 	
+	/**
+	 * @param aItem
+	 *            The item type.
+	 * @return the child item with the given item type.
+	 */
 	public ItemCreation getItem(Item aItem);
 	
 	/**
@@ -64,8 +97,16 @@ public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, Creati
 	 */
 	public List<ItemCreation> getItemsList();
 	
+	/**
+	 * @param aName
+	 *            The item name.
+	 * @return the value of the given child item name.
+	 */
 	public int getItemValue(String aName);
 	
+	/**
+	 * @return the group name.
+	 */
 	public String getName();
 	
 	/**
@@ -83,8 +124,18 @@ public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, Creati
 	 */
 	public int getValue();
 	
+	/**
+	 * @param aItem
+	 *            The item type.
+	 * @return whether this group contains an item with the given item type.
+	 */
 	public boolean hasItem(Item aItem);
 	
+	/**
+	 * @param aItem
+	 *            The item.
+	 * @return whether this group contains the given item.
+	 */
 	public boolean hasItem(ItemCreation aItem);
 	
 	/**
@@ -94,19 +145,29 @@ public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, Creati
 	 */
 	public boolean hasItem(String aName);
 	
+	/**
+	 * @param aItem
+	 *            The item child.
+	 * @return the index of the given item child.
+	 */
 	public int indexOfItem(ItemCreation aItem);
 	
-	public void init();
-	
+	/**
+	 * @return whether this group is mutable.
+	 */
 	public boolean isMutable();
 	
+	/**
+	 * @return whether this is a value group.
+	 */
 	public boolean isValueGroup();
 	
 	/**
-	 * Removes all widgets from their parent containers.
+	 * Removes the item with the given item type from this group.
+	 * 
+	 * @param aItem
+	 *            The item type.
 	 */
-	public void release();
-	
 	public void removeItem(Item aItem);
 	
 	/**
@@ -122,6 +183,14 @@ public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, Creati
 	 */
 	public void setCreationMode(CreationMode aMode);
 	
+	/**
+	 * Replaces the item child at the given index with the given item.
+	 * 
+	 * @param aIndex
+	 *            The item index.
+	 * @param aItem
+	 *            The new item.
+	 */
 	public void setItemAt(int aIndex, Item aItem);
 	
 	/**
@@ -132,17 +201,18 @@ public interface ItemGroupCreation extends Comparable<ItemGroupCreation>, Creati
 	 */
 	public void setPoints(PointHandler aPoints);
 	
+	/**
+	 * Updates whether the add child button is enabled.
+	 */
 	public void updateAddButton();
 	
+	/**
+	 * Updates the item controller.
+	 */
 	public void updateController();
 	
 	/**
 	 * Updates all values and whether they can be increased and decreased.
-	 * 
-	 * @param aCanIncrease
-	 *            Whether values can be increased.
-	 * @param aCanDecrease
-	 *            Whether values can be decreased.
 	 */
 	public void updateItems();
 }
