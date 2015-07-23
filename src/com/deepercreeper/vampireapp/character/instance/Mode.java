e03619e2f19b9b1d1276d8d495d3ab28a4b75d22
Vincent Reinthal <Vincent.R95@Gmail.com>
2015-07-23 22:02:18 +0200

    #118: Added mode settings and descriptions.

--- src/com/deepercreeper/vampireapp/character/instance/Mode.java
+++ src/com/deepercreeper/vampireapp/character/instance/Mode.java
@@ -10,15 +10,57 @@ public enum Mode
 	/**
 	 * Default state. Nothing special.
 	 */
-	DEFAULT,
+	DEFAULT(true, false, true),
 	
 	/**
 	 * The character is sleeping. No actions possible.
 	 */
-	SLEEPING,
+	SLEEPING(false, true, false),
 	
 	/**
 	 * The character has been hurt so much, that he can't move anymore.
 	 */
-	KO
+	KO(false, false, false),
+	
+	/**
+	 * The character is raging and can't handle some actions.
+	 */
+	RAGE(false, false, true);
+	
+	private final boolean mCanClientLeave;
+	
+	private final boolean mCanClientEnter;
+	
+	private final boolean mCanUseAction;
+	
+	private Mode(final boolean aCanClientLeave, final boolean aCanClientEnter, final boolean aCanUseActions)
+	{
+		mCanClientLeave = aCanClientLeave;
+		mCanClientEnter = aCanClientEnter;
+		mCanUseAction = aCanUseActions;
+	}
+	
+	/**
+	 * @return whether the client is able to enter this mode from every leavable other mode.
+	 */
+	public boolean canClientEnter()
+	{
+		return mCanClientEnter;
+	}
+	
+	/**
+	 * @return whether the client is able to leave this mode.
+	 */
+	public boolean canClientLeave()
+	{
+		return mCanClientLeave;
+	}
+	
+	/**
+	 * @return whether the client is able to use actions inside this mode.
+	 */
+	public boolean canUseAction()
+	{
+		return mCanUseAction;
+	}
 }
