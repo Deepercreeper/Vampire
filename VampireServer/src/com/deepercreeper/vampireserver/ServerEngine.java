87447fc19f4846b803236b8581c19725d9f79a6e
vrl <vrl@rb-lap-vrl.recommind.com>
2015-08-18 17:08:04 +0200

    #122: Added listening engine.

--- VampireServer/src/com/deepercreeper/vampireserver/ServerEngine.java
+++ VampireServer/src/com/deepercreeper/vampireserver/ServerEngine.java
@@ -1,7 +1,87 @@
 package com.deepercreeper.vampireserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerEngine
 {
-	public ServerEngine()
-	{}
+	private static final int PORT = 123;
+	
+	private final List<Socket> mSockets = new ArrayList<>();
+	
+	private final ServerFrame mFrame;
+	
+	private boolean mListening;
+	
+	private Listener mListener;
+	
+	private ServerSocket mSocket;
+	
+	public ServerEngine(ServerFrame aFrame)
+	{
+		mFrame = aFrame;
+		try
+		{
+			mSocket = new ServerSocket(PORT);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		if (mSocket != null)
+		{
+			mListener = new Listener();
+			mListener.start();
+		}
+	}
+	
+	private void addSocket(Socket aSocket)
+	{
+		mSockets.add(aSocket);
+		mFrame.setStatus("Added socket: " + aSocket.getInetAddress().getHostAddress());
+		try
+		{
+			Thread.sleep(100);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		mFrame.setStatus("Ready");
+	}
+	
+	public void stop()
+	{
+		mListening = false;
+		try
+		{
+			mSocket.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private class Listener extends Thread
+	{
+		@Override
+		public void run()
+		{
+			while (mListening)
+			{
+				try
+				{
+					addSocket(mSocket.accept());
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- VampireServer/src/com/deepercreeper/vampireserver/ServerFrame.java
+++ VampireServer/src/com/deepercreeper/vampireserver/ServerFrame.java
@@ -1,8 +1,15 @@
 package com.deepercreeper.vampireserver;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
 
 /**
  * This server handles the communication between vampire clients and hosts.
@@ -19,18 +26,38 @@ public class ServerFrame extends JFrame
 	
 	private final ServerEngine mEngine;
 	
+	private final JLabel mStatusLabel = new JLabel("Ready");
+	
 	public ServerFrame()
 	{
-		mEngine = new ServerEngine();
+		mEngine = new ServerEngine(this);
 		setSize(WIDTH, HEIGHT);
 		addWindowListener(new WindowAdapter()
 		{
 			@Override
 			public void windowClosing(WindowEvent e)
 			{
+				mEngine.stop();
 				System.exit(0);
 			};
 		});
+		init();
 		setVisible(true);
+	}
+	
+	public void setStatus(String aStatus)
+	{
+		mStatusLabel.setText(aStatus);
+	}
+	
+	private void init()
+	{
+		JPanel statusPanel = new JPanel();
+		statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		add(statusPanel, BorderLayout.SOUTH);
+		statusPanel.setPreferredSize(new Dimension(WIDTH, 16));
+		statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+		mStatusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		statusPanel.add(mStatusLabel);
 	}
 }
