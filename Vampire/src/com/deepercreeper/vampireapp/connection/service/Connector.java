30521eceb7cb7b5d0a4edd8eb06958443e5e7788
Vincent Reinthal <Vincent.R95@Gmail.com>
2015-09-16 16:22:24 +0200

    #108: Added java doc for connector.

--- Vampire/src/com/deepercreeper/vampireapp/connection/service/Connector.java
+++ Vampire/src/com/deepercreeper/vampireapp/connection/service/Connector.java
@@ -8,26 +8,77 @@ import com.deepercreeper.vampireapp.util.interfaces.Toaster;
 import android.app.Activity;
 import android.os.Handler;
 
+/**
+ * A connector is used to create connections to other servers and clients.
+ * 
+ * @author Vincent
+ */
 public interface Connector extends Toaster
 {
+	/**
+	 * Binds the default connector to the given activity.
+	 * 
+	 * @param aContext
+	 *            The underlying context.
+	 * @param aListener
+	 *            The connection listener.
+	 * @param aHandler
+	 *            The handler for toasting.
+	 */
 	public void bind(Activity aContext, ConnectionListener aListener, Handler aHandler);
 	
+	/**
+	 * Checks whether the current connection is active.
+	 */
 	public void checkActiveState();
 	
+	/**
+	 * @return whether the current connection is active.
+	 */
 	public boolean isActive();
 	
+	/**
+	 * @return whether this device is able to connect via network.
+	 */
 	public boolean hasNetwork();
 	
+	/**
+	 * @return whether this device is able the connect via WiFi direct.
+	 */
 	public boolean hasWifi();
 	
+	/**
+	 * Opens a dialog that makes is possible to choose a server.
+	 */
 	public void connect();
 	
+	/**
+	 * The given device has been connected.
+	 * 
+	 * @param aDevice
+	 *            The connected device.
+	 */
 	public void connectedTo(final ConnectedDevice aDevice);
 	
+	/**
+	 * Disconnects from the given device.
+	 * 
+	 * @param aDevice
+	 *            The device.
+	 */
 	public void disconnect(ConnectedDevice aDevice);
 	
+	/**
+	 * The connection to the given device was closed.
+	 * 
+	 * @param aDevice
+	 *            The disconnected device.
+	 */
 	public void disconnectedFrom(ConnectedDevice aDevice);
 	
+	/**
+	 * @return the current connection type.
+	 */
 	public ConnectionType getConnectionType();
 	
 	/**
@@ -35,6 +86,9 @@ public interface Connector extends Toaster
 	 */
 	public ConnectedDevice getHost();
 	
+	/**
+	 * @return whether this device is able to connect via bluetooth.
+	 */
 	public boolean hasBluetooth();
 	
 	/**
@@ -42,13 +96,43 @@ public interface Connector extends Toaster
 	 */
 	public boolean hasHost();
 	
+	/**
+	 * The given message was sent to this device.
+	 * 
+	 * @param aDevice
+	 *            The sender device.
+	 * @param aType
+	 *            The message type.
+	 * @param aArgs
+	 *            The message arguments.
+	 */
 	public void receiveMessage(final ConnectedDevice aDevice, final MessageType aType, final String[] aArgs);
 	
+	/**
+	 * Sends the given message to all connected devices.
+	 * 
+	 * @param aType
+	 *            The message type.
+	 * @param aArgs
+	 *            The message arguments.
+	 */
 	public void sendToAll(final MessageType aType, final String... aArgs);
 	
+	/**
+	 * Sets the new connection type.
+	 * 
+	 * @param aConnectionType
+	 *            The connection type.
+	 */
 	public void setConnectionType(ConnectionType aConnectionType);
 	
+	/**
+	 * Starts the server.
+	 */
 	public void startServer();
 	
+	/**
+	 * Unbinds this connector from the current activity.
+	 */
 	public void unbind();
 }
